a3a67b24000f6f410a126eca2474605b6451a47f
Thoralf-M <thoralf.mue@gmail.com>
2021-08-17 10:16:59 +0200

    update to neon 0.9

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const addon = require('./index.node');
 
-class WithEventQueue {
+class WithChannel {
     constructor(name) {
-        this.class = addon.with_event_queue_new(name);
+        this.class = addon.with_channel_new(name);
     }
     name() {
-        return addon.with_event_queue_name.apply(this.class);
+        return addon.with_channel_name.apply(this.class);
     }
 }
 
-class WithoutEventQueue {
+class WithoutChannel {
     constructor(name) {
-        this.class = addon.without_event_queue_new(name);
+        this.class = addon.without_channel_new(name);
     }
     name() {
-        return addon.without_event_queue_name.apply(this.class);
+        return addon.without_channel_name.apply(this.class);
     }
 }
 
 // without event queue works fine
 // setTimeout(() => {
-//     console.log('Finished:', new WithoutEventQueue('Test').name())
+//     console.log('Finished:', new WithoutChannel('Test').name())
 //     // }, 100); // small timeout, process will exit
 // }, 10000); // timeout around 10 seconds or higher, process will also exit
 
 // with event queue it hangs after > ~10 seconds timeout
 setTimeout(() => {
-    console.log('Finished:', new WithEventQueue('Test').name())
+    console.log('Finished:', new WithChannel('Test').name())
     // }, 100); // small timeout, process will exit
 }, 10000); // timeout around 10 seconds or higher, process will never exit
--- src/lib.rs
+++ src/lib.rs
@@ -3,69 +3,69 @@ use std::sync::Arc;
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("with_event_queue_new", with_event_queue_new)?;
-    cx.export_function("with_event_queue_name", with_event_queue_name)?;
-    cx.export_function("without_event_queue_new", without_event_queue_new)?;
-    cx.export_function("without_event_queue_name", without_event_queue_name)?;
+    cx.export_function("with_channel_new", with_channel_new)?;
+    cx.export_function("with_channel_name", with_channel_name)?;
+    cx.export_function("without_channel_new", without_channel_new)?;
+    cx.export_function("without_channel_name", without_channel_name)?;
     Ok(())
 }
 
-pub struct WithEventQueue {
-    queue: EventQueue,
+pub struct WithChannel {
+    channel: Channel,
     name: String,
 }
 
-impl Finalize for WithEventQueue {}
+impl Finalize for WithChannel {}
 
-impl WithEventQueue {
-    fn new(queue: EventQueue, name: String) -> Arc<Self> {
-        Arc::new(Self { queue, name })
+impl WithChannel {
+    fn new(channel: Channel, name: String) -> Arc<Self> {
+        Arc::new(Self { channel, name })
     }
 }
 
-pub fn with_event_queue_new(mut cx: FunctionContext) -> JsResult<JsBox<Arc<WithEventQueue>>> {
+pub fn with_channel_new(mut cx: FunctionContext) -> JsResult<JsBox<Arc<WithChannel>>> {
     let name = cx.argument::<JsString>(0)?;
     let name = name.value(&mut cx);
-    let queue = cx.queue();
-    let wrapper = WithEventQueue::new(queue, name);
+    let channel = cx.channel();
+    let wrapper = WithChannel::new(channel, name);
 
     Ok(cx.boxed(wrapper))
 }
 
-pub fn with_event_queue_name(mut cx: FunctionContext) -> JsResult<JsString> {
+pub fn with_channel_name(mut cx: FunctionContext) -> JsResult<JsString> {
     let wrapper = Arc::clone(
         &&cx.this()
-            .downcast_or_throw::<JsBox<Arc<WithEventQueue>>, FunctionContext>(&mut cx)?,
+            .downcast_or_throw::<JsBox<Arc<WithChannel>>, FunctionContext>(&mut cx)?,
     );
     let name = wrapper.name.clone();
 
     Ok(cx.string(&name))
 }
 
-pub struct WithoutEventQueue {
+pub struct WithoutChannel {
     name: String,
 }
 
-impl Finalize for WithoutEventQueue {}
+impl Finalize for WithoutChannel {}
 
-impl WithoutEventQueue {
+impl WithoutChannel {
     fn new(name: String) -> Arc<Self> {
         Arc::new(Self { name })
     }
 }
 
-pub fn without_event_queue_new(mut cx: FunctionContext) -> JsResult<JsBox<Arc<WithoutEventQueue>>> {
+pub fn without_channel_new(mut cx: FunctionContext) -> JsResult<JsBox<Arc<WithoutChannel>>> {
     let name = cx.argument::<JsString>(0)?;
     let name = name.value(&mut cx);
-    let wrapper = WithoutEventQueue::new(name);
+    let wrapper = WithoutChannel::new(name);
 
     Ok(cx.boxed(wrapper))
 }
 
-pub fn without_event_queue_name(mut cx: FunctionContext) -> JsResult<JsString> {
+pub fn without_channel_name(mut cx: FunctionContext) -> JsResult<JsString> {
     let wrapper = Arc::clone(
         &&cx.this()
-            .downcast_or_throw::<JsBox<Arc<WithoutEventQueue>>, FunctionContext>(&mut cx)?,
+            .downcast_or_throw::<JsBox<Arc<WithoutChannel>>, FunctionContext>(&mut cx)?,
     );
     let name = wrapper.name.clone();
 
